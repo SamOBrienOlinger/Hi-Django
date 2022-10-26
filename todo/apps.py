bcba0502b7ade6a1a4ca3cae28cb42eeebe053b3
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-10-26 09:26:18 +0000

    Try to fix mess

--- todo/apps.py
+++ todo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppNameConfig(AppConfig):
+class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'APP_NAME'
+    name = 'todo'
